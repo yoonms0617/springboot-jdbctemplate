422086dc8aa2688704ecc457508d9281a13cd3fe
yoonms0617 <yoonminwater@gmail.com>
2022-04-21 10:50:56 +0900

    Initial commit

---
+++ src/main/java/com/example/springjdbc/SpringjdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjdbcApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER(user_name, user_email) VALUES ('kimkun', 'kim@test.com');
+INSERT INTO USER(user_name, user_email) VALUES ('yoonkun', 'yoon@test.com');
+INSERT INTO USER(user_name, user_email) VALUES ('parkkun', 'park@test.com');
